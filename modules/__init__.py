cb18c00bf777b8ccf462daafaeccc22e53be5283
Luis Banuelos Chacon <luis.banuelos.chacon@hp.com>
2016-10-31 17:18:51 -0700

    10/31/2016

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,2 @@
 from .galil_wrapper import GalilController, GalilAxis
+from .program_thread import ProgramThread
--- views/__init__.py
+++ views/__init__.py
@@ -1,16 +1,23 @@
+# UI File Importer
 from PyQt4 import uic
 from glob import glob
 from os.path import split, join
 
 # Stores all loaded UI classes by name
-ViewBase = {}
-ViewForm = {}
+View = {}
 
 # Load UI files
 path = split(__file__)[0]
 for ui in glob(join(path, '*.ui')):
     base, form = uic.loadUiType(ui)
-    name = split(ui)[1][:-3]
 
-    ViewBase[name] = base
-    ViewForm[name] = form
+    # get name in CamelCase
+    name = split(ui)[1][:-3]
+    name = name.title().replace(' ', '').replace('_', '')
+
+    View[name] = type(name, (base, form), {})
+
+# Import classes
+from .program_tab import ProgramTab
+from .connection_tab import ConnectionTab
+from .axis_manual_control import AxisManualControl
